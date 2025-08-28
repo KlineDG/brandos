d7e8c63c8d1b7c00822ea19f1fa356398ee8959d
Dylan <Klinedg9@icloud.com>
2025-08-28 13:50:00 -0600

    Very minor fix awaited cookies in layout.tsx (protected)

--- front-end/app/(protected)/layout.tsx
+++ front-end/app/(protected)/layout.tsx
@@ -16,7 +16,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
